08fb0aa0aadca3e04976975713a997b4863d2a1b
Cleudeir <Cleudeirsilva@gmail.com>
2023-03-31 12:08:10 -0300

    start

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  reactStrictMode: false,
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/languages',
+        permanent: true,
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig
